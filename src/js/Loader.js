314927f6459b21755e641f8fc9fc8e3827adc9e7
dm <3004dm@gamil.com>
2025-01-29 16:01:02 +0200

    [removed]: redurant variable 'loadCount'

--- src/js/Loader.js
+++ src/js/Loader.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three'
+
 import image1 from '../../static/textures/01.webp'
 import image2 from '../../static/textures/02.webp'
 import image3 from '../../static/textures/03.webp'
@@ -7,16 +8,14 @@ import image4 from '../../static/textures/04.webp'
 export default class Loader {
     loadTextures(onComplete) {
         const textureLoader = new THREE.TextureLoader()
-        let loadCount = 0
         const imageSources = [image1, image2, image3, image4]
         const textures = []
         
         imageSources.forEach((src, index) => {
             const onLoad = (texture) => {
                 textures[index] = texture
-    
-                loadCount += 1
-                if (loadCount === imageSources.length) {
+
+                if (index === imageSources.length - 1) {
                     onComplete(textures)
                 }
             }
